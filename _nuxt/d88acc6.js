017a3138a9143854bc789b3c851716c3942a083d
elirehema <elirehemapaul@yahoo.com>
2020-12-29 20:21:39 +0000

    Deploying to gh-pages from  @ 14845001be26da4908040e3e00b883cd275982d0 🚀

--- _nuxt/d88acc6.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{377:function(t,e,n){"use strict";n.r(e);var l=n(18),component=Object(l.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("v-card",{staticClass:"pa-3"},[e("h1",[this._v("Laboratories and its activities will be Displayed Here")])])}),[],!1,null,null,null);e.default=component.exports}}]);
